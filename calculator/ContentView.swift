da0add83ac2448018e3535364bcd84f904f9c6fd
kiumars_ch <kiumarsch96@gmail.com>
2023-08-24 13:35:23 +0330

    added formatting for the number

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -107,7 +107,10 @@ struct ContentView: View {
         if currentMode == .notSet{
             savedNum = currentValueInt
         }
-        currentValue = "\(currentValueInt)"
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let num = NSNumber(value: currentValueInt)
+        currentValue = numberFormatter.string(from: num) ?? "Error!"
         
     }
 }
